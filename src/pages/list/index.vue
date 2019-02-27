47099274b3c110025e3504f72a76b13b23a0c898
Allison12345 <2562392373@qq.com>
2019-02-27 16:39:47 +0800

    list

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -9,6 +9,31 @@ div
         p(slot='bd') 标题文字
       .weui-cell__ft(slot='ft')
         weui-swiped(v-if='isShow' warn='warn' href='javascript:') 删除
+  weui-cells(title='带图标、说明的列表项')
+  weui-cells
+    weui-cell(ft='说明文字')
+      .weui-cell(slot='hd' )
+        img
+      p.weui-cell(slot='bd') 标题
+    weui-cell(ft='说明文字')
+      .weui-cell(slot='hd' )
+        img
+      p.weui-cell(slot='bd') 标题
+  weui-cells(title='带跳转的列表项')
+  weui-cells
+    weui-cell(access='access' :hd='null' @click='onClick()')
+      .weui-cell(slot='bd') cell standard
+      .weiu-cell(slot='ft')
+  weui-cells(title='带说明,跳转的列表项')
+  weui-cells
+    weui-cell(access='access' :hd='null' ft='说明文字' @click='onClick()')
+      p.weui-cell(slot='bd') 
+        a cell standard
+  weui-cells(title='带图标,说明,跳转的列表项' ft='说明文字' )
+    weui-cell(access='access' @click='onClick()')
+      .weui-cell(slot='hd') 
+        img
+      p.weui-cell(slot='bd') cell standard
 </template>
 <script>
 export default {
@@ -19,6 +44,9 @@ export default {
   methods: {
     toggleShow() {
       this.isShow = !this.isShow
+    },
+    onClick() {
+      window.open('https://www.baidu.com/')
     }
   }
 }
