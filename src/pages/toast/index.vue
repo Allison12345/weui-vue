7d79a9ea6e7f97a44afe1941f9d1166bc3898b4d
Allison12345 <2562392373@qq.com>
2019-05-18 15:45:06 +0800

    toast

--- src/pages/toast/index.vue
+++ src/pages/toast/index.vue
@@ -4,18 +4,20 @@
       h1.page__title Toast
       p.page__desc 弹出式提示
     .page__bd.page__bd_spacing
-      weui-button(type='default' @click='isShown=true') 成功提示
-      weui-button(type='default' @click='isShown=true') 加载中提示
+      weui-button(type='default' @click='isShown=true;finish=true;onClick()' ) 成功提示
+      weui-button(type='default' @click='isShown=true;loading=true;onClick()' ) 加载中提示
     weui-mask-transparent(v-if='isShown' )
-      weui-toast
-      weui-loading-toast
+      weui-toast(v-if='finish')
+      weui-loading-toast(v-if='loading')
 </template>
 <script>
 export default {
   name: 'toast',
   data() {
     return {
-      isShown: false
+      isShown: false,
+      finish: false,
+      loading: false
     }
   },
   methods: {
@@ -23,6 +25,8 @@ export default {
       console.log('success')
       setTimeout(() => {
         this.isShown = false
+        this.finish = false
+        this.loading = false
       }, 3000)
     }
   }
